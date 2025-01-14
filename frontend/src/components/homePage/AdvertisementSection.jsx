7a39f19ded80813de1916b846510c1b920cb3169
Asif Hossain <asifiscoding@gmail.com>
2025-01-14 08:46:00 +0600

    fix (Advertisement.jsx): image set to lazy load

--- frontend/src/components/homePage/AdvertisementSection.jsx
+++ frontend/src/components/homePage/AdvertisementSection.jsx
@@ -6,7 +6,7 @@ const AdvertisementSection = () => {
         <div className="container">
             <div className="d-flex align-items-center">
                 <div className="app-img">
-                    <img className="img-fluid phone" src="assets/images/service-phone.png" alt="app-phone"/>
+                    <img className="img-fluid phone" src="assets/images/service-phone.png" alt="app-phone" loading="lazy"/>
                 </div>
                 <div className="app-content">
                     <h2>Zomo App : Online & Mobile Ordering</h2>
@@ -16,11 +16,11 @@ const AdvertisementSection = () => {
                     </h5>
                     <div className="app-buttons d-flex align-items-center gap-3">
                         <a href="https://www.apple.com/in/app-store/">
-                            <img className="img-fluid app-btn" src="assets/images/svg/app-store.svg" alt="app-store"/>
+                            <img className="img-fluid app-btn" src="assets/images/svg/app-store.svg" alt="app-store"  loading="lazy" />
                         </a>
                         <a href="https://play.google.com/store/apps">
                             <img className="img-fluid app-btn" src="assets/images/svg/google-play.svg"
-                                alt="google-play" />
+                                alt="google-play" loading="lazy" />
                         </a>
                     </div>
                 </div>
