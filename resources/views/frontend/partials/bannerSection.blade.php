e2e51e9f1c2da6f80a5f7a6882a06aa18ccbcce9
Asif Hossain <asifiscoding@gmail.com>
2025-01-22 15:37:58 +0600

    feat (home page) : all section added

---
+++ resources/views/frontend/partials/bannerSection.blade.php
@@ -0,0 +1,53 @@
+<section class="pt-4 home3">
+  <div class="custom-container">
+      <div class="position-relative">
+        {{-- "{{asset('assets/frontend/frontend/css/style.css')}}" --}}
+          <img src="{{asset('assets/frontend/images/home-bg2.png')}}" class="img-fluid bg-home-img" alt="">
+          <div class="home-content">
+              <div class="row w-100 h-100">
+                  <div class="col-sm-6 col-12">
+                      <div class="home-left-content">
+                          <label>50% off on First delivery</label>
+                          <h2>Made with love,<br />Savored with interest.</h2>
+                          <p>Browse out top categories here to discover different food cuision.
+                          </p>
+                          <div class="search-section">
+                              <form class="auth-form search-head" target="_blank">
+                                  <div class="form-group">
+                                      <div class="form-input mb-0">
+                                          <input type="search" class="form-control search" id="inputusername"
+                                                 placeholder="Search for Restaurant">
+                                          <i class="ri-search-line search-icon"></i>
+                                      </div>
+                                  </div>
+                              </form>
+                              <a class="btn theme-btn mt-0" href="#" role="button">Search</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-sm-6 d-sm-block d-none">
+                      <div class="home-right position-relative">
+                          <img src="{{asset('assets/frontend/images/mobile.png')}}" class="img-fluid base-phone" alt="">
+                          <div class="animated-img">
+                              <div class="food1">
+                                  <img src="{{asset('assets/frontend/images/food1.png')}}" data-aos="fade-down" data-aos-easing="linear"
+                                       data-aos-anchor-placement="top-center" data-aos-duration="1200"
+                                       class="img-fluid" alt="">
+                              </div>
+                              <div class="food2">
+                                  <img src="{{asset('assets/frontend/images/food2.png')}}" data-aos-duration="1200" data-aos="fade-down"
+                                       data-aos-anchor-placement="bottom-center" class="img-fluid" alt="">
+                              </div>
+                              <div class="food3">
+                                  <img src="{{asset('assets/frontend/images/food3.png')}}" data-aos="fade-down" data-aos-easing="linear"
+                                       data-aos-anchor-placement="bottom-bottom" data-aos-duration="1000"
+                                       class="img-fluid" alt="">
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>
---
+++ resources/views/frontend/partials/popularRestaurantSection.blade.php
@@ -0,0 +1,290 @@
+<>
+{{-- title --}}
+<div class="container">
+  <div class="title-center">
+      <h2>Tasty food to your destination <span> in 20 mins.</span></h2>
+  </div>
+</div>
+
+{{-- content --}}
+<section class="popular-restaurant section-b-space ratio3_2 overflow-hidden">
+  <div class="container">
+      <div class="row g-md-4 g-3">
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-1.png')}}"
+                               alt="vp1">
+                      </a>
+                      <div class="offers">
+                          <img src="{{asset('assets/frontend/images/shape.png')}}" alt="shape" class="img-fluid">
+                          <h6>upto $2</h6>
+                          <div class="d-flex align-items-center justify-content-end">
+                              <h4>50% OFF</h4>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">Poultry Palace</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.9
+                          </h6>
+                      </div>
+                      <h5 class="product-items">Chicken quesadilla, avocado....</h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">New Jsercy</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 3.2 km</li>
+                              <li><i class="ri-time-fill icon"></i> 25 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="vertical-product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-2.png')}}"
+                               alt="vp-2">
+                      </a>
+                      <div class="offers">
+                          <img src="{{asset('assets/frontend/images/shape.png')}}" alt="shape" class="img-fluid">
+                          <h6>upto $2</h6>
+                          <div class="d-flex align-items-center justify-content-end">
+                              <h4>50% OFF</h4>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">Ribeye Junction</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.2
+                          </h6>
+                      </div>
+                      <h5 class="product-items">Chicken quesadilla, avocado....</h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">California</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 1 km</li>
+                              <li><i class="ri-time-fill icon"></i> 10 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-3.png')}}"
+                               alt="vp3">
+                      </a>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">
+                                  The Grill Master's Cafe
+                              </h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>4.3
+                          </h6>
+                      </div>
+                      <h5 class="product-items">Bread, Eggs, Butter, Fries...</h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">New York</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 5 km</li>
+                              <li><i class="ri-time-fill icon"></i> 40 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-4.png')}}"
+                               alt="vp-4">
+                      </a>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">Cozy Cuppa Cafe</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.6
+                          </h6>
+                      </div>
+                      <h5 class="product-items">cheesecake, waffles, Cakes,...</h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">Dallas</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 4 km</li>
+                              <li><i class="ri-time-fill icon"></i> 30 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-5.png')}}"
+                               alt="vp-5">
+                      </a>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">Mocha Magic Cafe</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.2
+                          </h6>
+                      </div>
+                      <h5 class="product-items">Chinese, Momos, Dumplings,...</h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">Seattle</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 1 km</li>
+                              <li><i class="ri-time-fill icon"></i> 8 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-6.png')}}"
+                               alt="vp16">
+                      </a>
+                      <div class="offers">
+                          <img src="{{asset('assets/frontend/images/shape.png')}}" alt="shape" class="img-fluid">
+                          <h6>upto $2</h6>
+                          <div class="d-flex align-items-center justify-content-end">
+                              <h4>50% OFF</h4>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">Latte Lounge</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.6
+                          </h6>
+                      </div>
+                      <h5 class="product-items">
+                          Chicken fingers, Chicken goujons,....
+                      </h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">Atlanta</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 3 km</li>
+                              <li><i class="ri-time-fill icon"></i> 25 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="vertical-product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-7.png')}}"
+                               alt="vp-7">
+                      </a>
+                      <div class="offers">
+                          <img src="{{asset('assets/frontend/images/shape.png')}}" alt="shape" class="img-fluid">
+                          <h6>upto $2</h6>
+                          <div class="d-flex align-items-center justify-content-end">
+                              <h4>50% OFF</h4>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">The Burger Barn</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.8
+                          </h6>
+                      </div>
+                      <h5 class="product-items">
+                          Burger, Garlic Bread, Sandwich....
+                      </h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">Chicago</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 2.4 km</li>
+                              <li><i class="ri-time-fill icon"></i> 20 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="vertical-product-box product-style-3">
+                  <div class="vertical-product-box-img">
+                      <a href="menu-listing.html">
+                          <img class="vertical-product-img-top w-100 bg-img" src="{{asset('assets/frontend/images/product/vp-8.png')}}"
+                               alt="vp-8">
+                      </a>
+                      <div class="offers">
+                          <img src="{{asset('assets/frontend/images/shape.png')}}" alt="shape" class="img-fluid">
+                          <h6>upto $2</h6>
+                          <div class="d-flex align-items-center justify-content-end">
+                              <h4>50% OFF</h4>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="vertical-product-body">
+                      <div class="d-flex align-items-center justify-content-between">
+                          <a href="menu-listing.html">
+                              <h4 class="vertical-product-title">Wing Master</h4>
+                          </a>
+                          <h6 class="rating-star">
+                              <span class="star"><i class="ri-star-s-fill"></i></span>3.2
+                          </h6>
+                      </div>
+                      <h5 class="product-items">Chicken quesadilla, avocado....</h5>
+                      <div
+                          class="location-distance d-flex align-items-center justify-content-between pt-sm-3 pt-2">
+                          <h5 class="place">New York</h5>
+                          <ul class="distance">
+                              <li><i class="ri-map-pin-fill icon"></i> 1.8 km</li>
+                              <li><i class="ri-time-fill icon"></i> 12 min</li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>
+
+</>
